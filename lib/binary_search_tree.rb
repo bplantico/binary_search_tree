d1828d7c73328bf7792a08d760b9c38074bd9880
Brian Plantico <bplantico@gmail.com>
2019-07-15 11:02:29 -0600

    Charred Binary Search Tree

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,27 +1,32 @@
 class BinarySearchTree
-  attr_reader :tree
+  attr_reader :tree,
+              :depth
 
   def initialize
     @tree = []
-    @tree_depth = 0
   end
 
   def insert(position, movie)
 
-    if @tree.count == 0
-      @tree_depth
-    elsif @tree.count <= 3
-      @tree_depth += 1
-    elsif @tree.count <= 7
-      @tree_depth += 1
-    elsif @tree.count <= 15
-      @tree_depth += 1
-    end
-
     node = Hash.new
     node[movie] = position
+
+    # Need a way to track 1) the count and 2) the previous node's value.
+    # iterate through in order to check if the current node is greater or less than previous node.
+    # If the current node has a greater value than previous node, then
+    # the depth should stop counting (also true if there's not a previous node)
+    previous_node_position = @tree.last.values[0]
+
+    depth = 0
+    if previous_node.nil? || position <
+      depth
+    elsif
+      depth += 1
+    end
+
     @tree << node
-    @tree_depth
+
+    depth
   end
 
 end
--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -10,13 +10,16 @@ class BinarySearchTreeTest < Minitest::Test
     assert_instance_of BinarySearchTree, @tree
   end
 
+  def test_its_depth_initializes_as_0
+    assert 0, @tree.depth
+  end
+
   def test_it_can_insert_nodes
     assert_equal 0, @tree.insert(61, "Bill & Ted's Excellent Adventure")
     assert_equal 1, @tree.insert(16, "Johnny English")
     assert_equal 1, @tree.insert(92, "Sharknado 3")
-    assert_equal 2, @tree.insert(50, "Hannibal Buress: Animal Furnace")
+    require "pry"; binding.pry
+    # assert_equal 2, @tree.insert(50, "Hannibal Buress: Animal Furnace")
   end
-
-
 
 end
